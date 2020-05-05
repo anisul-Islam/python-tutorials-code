30c6d2f77665e3ec9a840cec2a4f9834c0e8ff68
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-06 01:29:32 +0300

    Comments and backslash characters

---
+++ Program2.py
@@ -0,0 +1,13 @@
+# This is a single line comment
+# Backslash characters and comments
+'''
+    This
+    is an
+    example of multiple line comment
+
+'''
+# print("Name : Anisul Islam")
+# print("Address : Sylhet, Bangladesh")
+# print("01710444700")
+print("Name : Anisul Islam \nAddress : Sylhet, Bangladesh\n01710444700")
+print("1\t2")

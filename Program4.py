e7c5c396586bae6a659bf3368b8295485ca8a3b3
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 14:50:01 +0300

    Basic Numerical operations

---
+++ Program4.py
@@ -0,0 +1,24 @@
+# Basic Numerical operations -> +, -, *, /, % (modulus) , // (floor) , ** (exponential)
+num1 = 5
+num2 = 4
+
+result = num1 + num2
+print("Addition =  ", result)
+
+result = num1 - num2
+print("Subtraction =  ", result)
+
+result = num1 * num2
+print("Multiplication =  ", result)
+
+result = num1 / num2
+print("Division =  ", result)
+
+result = num1 % num2
+print("Remainder =  ", result)
+
+result = num1 ** num2
+print("Exponential =  ", result)
+
+result = num1 // num2
+print("Floor =  ", result)

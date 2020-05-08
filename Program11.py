bd14034e8fc2b8e59695cb6a92aeea9468441902
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 23:51:26 +0300

    if else : pass/fail, even/odd program

---
+++ Program11.py
@@ -0,0 +1,15 @@
+# conditional statement - > if, else statement
+
+# pass/fail program
+marks = int(input("Enter your marks : "))
+if marks >= 33:
+    print("Pass")
+else:
+    print("Fail")
+
+# even/odd program
+num = int(input("Enter a number : "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")

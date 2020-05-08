da616d4efd3bf9dfa6b3e626042d7c5206377b1b
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 23:55:52 +0300

    break and continue keyword

---
+++ Program19.py
@@ -0,0 +1,21 @@
+# break and continue keyword
+
+# break program
+print("Break program : ", end="")
+i = 1
+while i <= 100:
+    if i == 20:
+        break
+    print(i, end=" ")
+    i = i + 1
+
+'''
+# continue program
+print("Continue program : ", end="")
+i = 1
+while i <= 100:
+    if i == 20:
+        continue
+    print(i, end=" ")
+    i = i + 1
+'''

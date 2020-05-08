7995160b56a5fa849e3a04d7526e164991735e6a
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 14:50:56 +0300

    Getting user input

---
+++ Program5.py
@@ -0,0 +1,11 @@
+# Getting User input
+
+name = input("Enter your Name : ")
+age = input("Enter your Age : ")
+cgpa = input("Enter your CGPA : ")
+
+print("Student Information")
+print("---------------------")
+print("Name : "+ name)
+print("Age :  "+ age)
+print("CGPA : "+ cgpa)

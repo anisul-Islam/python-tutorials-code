bceeaa0dadd5f60368221fe4d37358bf8add8782
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 15:18:41 +0300

    Math related library functions

---
+++ Program8.py
@@ -0,0 +1,10 @@
+# Math Related Library functions
+from math import *
+print(max(5,3))
+print(min(5,3))
+print(pow(5,3))
+print(sqrt(25))
+print(round(5.3))
+print(round(5.8))
+print(floor(5.3))
+print(ceil(5.3))

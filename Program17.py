b9d1aa1a67f6907a957e637114dbe971508d38b8
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 23:55:08 +0300

    Loop control statement -> while loop

---
+++ Program17.py
@@ -0,0 +1,23 @@
+# Loop control statement -> while loop, for loop
+# while loop
+
+# print 1 to 100
+num = 1
+while num <= 100:
+    print(num, end=" ")
+    num = num + 1
+print("end")
+
+# print all the even numbers from 1 to 100
+num = 2
+while num <= 100:
+    print(num, end=" ")
+    num = num + 2
+print("end")
+
+# print all the odd numbers from 1 to 100
+num = 1
+while num <= 100:
+    print(num, end=" ")
+    num = num + 2
+print("end")

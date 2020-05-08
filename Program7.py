32ab2f74963a58269ee7a63bec0cce6c5e42fff2
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-08 14:55:09 +0300

    Area of a shape

---
+++ Program7.py
@@ -0,0 +1,12 @@
+# Area of a shape
+
+# Area of triangle
+base = float(input("Enter base = "))
+height = float(input("Enter height = "))
+area = 0.5 * base * height
+print("Area of triangle = ", area)
+
+# Area of circle
+radius = float(input("Enter radius = "))
+area = 3.1416 * radius * radius
+print("Area of circle = ", area)

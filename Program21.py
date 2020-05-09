5e555ac0ea9928daa923e3bcdd3457eb22db999e
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-09 11:35:59 +0300

    range function in python

---
+++ Program21.py
@@ -0,0 +1,13 @@
+# range function in python
+# range will generated a set of numbers
+numbers = list(range(10))
+print(numbers)
+print(numbers[2])
+
+# starting and end point can be set
+numbers = list(range(5, 11))
+print(numbers)
+
+# starting, end point and interval can be set
+numbers = list(range(2, 100, 2))
+print(numbers)

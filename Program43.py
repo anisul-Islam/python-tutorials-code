482bbe3b500bacc4b3f9fce3b285182c768762fd
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-12 16:24:31 +0300

    How to write in a file

---
+++ Program43.py
@@ -0,0 +1,36 @@
+# Exceptions Handling -> try, except, finally
+# Run time errors, compile time errors
+
+'''
+# ZeroDivisionError -> 20 / 0
+# TypeError
+# ValueEroor -> 20 / a
+# IndexError -> x = [10,20], print(x[3])
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+result = num1 / num2
+print(result)
+print("Done")
+
+
+test = "Anis"
+print(test[2])
+print(test[4])
+print("Done")
+
+'''
+try:
+    list = [20,0,30]
+    # result = list[0] / list[1]
+    result = list[0] / list[3]
+    print(result)
+
+except ZeroDivisionError:
+    print("Dividing by 0 is not possible")
+
+except IndexError:
+    print("Index is not found")
+
+finally:
+    print("done")

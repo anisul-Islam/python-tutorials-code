4c9f53ec0b7af9f2a792d0916090f7b16f4eba78
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-12 15:34:01 +0300

    introduction to function

---
+++ Program32.py
@@ -0,0 +1,26 @@
+# introduction to function
+
+
+def add1(a, b):
+    sum = a + b
+    print(sum)
+
+
+def add2(a, b, c):
+    sum = a + b + c
+    print(sum)
+
+
+def sub(a, b):
+    result = a - b
+    print(result)
+
+
+def message():
+    print("hello everyone")
+
+
+add1(10, 20)
+add2(10, 20, 30)
+sub(20, 10)
+message()

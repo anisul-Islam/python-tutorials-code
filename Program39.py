93dd3c9b01883c684c13b96d30df4b87e8d7d51a
anisul-Islam <anisul2010s@yahoo.co.uk>
2020-05-12 15:39:41 +0300

    zip function

---
+++ Program39.py
@@ -0,0 +1,7 @@
+# Zip Function
+roll = [101,102,103]
+name = ["Anisul Islam", "Rabeya Begum", "Linkon Talukdar"]
+# (roll,name)
+# combine 2 list and make one using zip function
+print(list(zip(roll,name)))
+print(list(zip(roll,name,"ABC")))
